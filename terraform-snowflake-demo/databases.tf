62ac8339354c5bb7f9bd4f91c36d1bee9dce5492
James Bada Sopkin <69161193+Bada-S@users.noreply.github.com>
2023-07-18 06:30:01 -0500

    Delete terraform-snowflake-demo directory

--- terraform-snowflake-demo/databases.tf
+++
@@ -1,6 +0,0 @@
-# snowflake_database.raw_fivetran will be destroyed
-resource "snowflake_database" "raw_fivetran" {
-  comment                     = "Unmodified source replicated data"
-  data_retention_time_in_days = 1
-  name                        = "RAW_DB_FIVETRAN"
-}
